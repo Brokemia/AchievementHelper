fdf3ab68c9111f0f97c6f74ad10037c0d7eb53f1
Brokemia <37253216+Brokemia@users.noreply.github.com>
2022-10-04 23:06:17 -0400

    Added localization for menu binding and added Chinese translation

--- AchievementHelperModuleSettings.cs
+++ AchievementHelperModuleSettings.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 namespace Celeste.Mod.AchievementHelper {
     public class AchievementHelperModuleSettings : EverestModuleSettings {
 
+        [SettingName("AchievementHelper_UI_MenuBinding")]
         public ButtonBinding AchievementMenuBinding { get; set; } = new(Microsoft.Xna.Framework.Input.Buttons.Back, Microsoft.Xna.Framework.Input.Keys.Back);
 
     }
